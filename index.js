33d58ce62b1bc4dc8a32ecfc9187f1cc40aa41e7
rushi <rushikesh.tokle@gmail.com>
2025-08-05 13:15:47 +0530

    action performed on database using Get

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const {faker}=require("@faker-js/faker");
 const mysql=require('mysql2');
+const express=require("express");
+const app=express();
+const port=8080;
+const path =require("path");
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -21,22 +31,51 @@ let  getRandomUser=()=> {
 
 
 /////inserting new data
-let q="insert into user(id, username, email, password) values ?";
 
-let data=[];
-for(let i=1;i<=100;i++){
-   data.push(getRandomUser());
-}
 
-try{
-connection.query(q,[data],(err,result)=>{
-    if(err) throw err;
-    console.log(result);
+// try{
+// connection.query(q,[data],(err,result)=>{
+//     if(err) throw err;
+//     console.log(result);
    
-})
-}catch(err){
+// })
+// }catch(err){
+//     console.log(err);
+// }
+//  connection.end();
+
+
+///home Route
+app.get("/",(req,res)=>{
+   let q="select count(*) from user";
+   try{
+    connection.query(q,(err,result)=>{
+        if(err) throw err;
+      console.log(result[0]["count(*)"]);
+      let count=result[0]["count(*)"];
+      res.render("home.ejs",{count});
+    })
+   }catch(err){
     console.log(err);
-}
- connection.end();
+    res.send("Some error occured");
+   }
+})
+
+//Show route
+app.get("/user",(req,res)=>{
+    let q="select * from user";
+    try{
+        connection.query(q,(err,result)=>{
+            if(err) throw err;
+          let data=result;
+          res.render("showUsers.ejs",{data});
+        })
+    }catch(err){
+        res.render("Error Ocured");
+    }
+})
 
 
+app.listen(port,()=>{
+console.log(`The app is listening on ${port}`);
+})
